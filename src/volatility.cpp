2a2609eeb10df066403b671bc41698c2037bf692
thomasdgm <thomas@degremont.me>
2026-05-28 10:51:38 +0200

    Update volatility.cpp

--- src/volatility.cpp
+++ src/volatility.cpp
@@ -9,7 +9,7 @@ namespace volatility{
     double implied_volatility_Call(const double S, const double K, const double r, const double T, const double market_price){
         double sigma = 0.1;
         double BS = blackscholes::calculate_pricing_Call(S,K,r,sigma,T);
-        double f = 1;
+        double f = BS - market_price;
         double v = 1;
         int cmp = 0;
         while(abs(f) > pow(10.0,-6.0)){
@@ -43,7 +43,7 @@ namespace volatility{
     double implied_volatility_Put(const double S, const double K, const double r, const double T, const double market_price){
         double sigma = 0.1;
         double BS = blackscholes::calculate_pricing_Put(S,K,r,sigma,T);
-        double f = 1;
+        double f = BS - market_price;
         double v = 1;
         int cmp = 0;
         while(f > pow(10.0,-6.0)){
